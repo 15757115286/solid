b84d1468a53ac772e6e0e51e9d68b924a3c320b3
15757115286 <2270532540@qq.com>
2018-07-24 22:14:46 +0800

    treeCompont修复

--- src/app/page/administer/Component1/index.vue
+++ src/app/page/administer/Component1/index.vue
@@ -1,5 +1,38 @@
 <template>
     <div>
         <span>这个是测试组件1</span>
+        <Modal
+            title="Title"
+            v-model="show"
+            :mask-closable="false">
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+            <input type="text" v-model="name">
+            <p>{{ name }}</p>
+        </Modal>
+         <Modal
+            title="Title"
+            v-model="show2"
+            :mask-closable="false">
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+            <input type="text" v-model="name">
+            <p>{{ name }}</p>
+        </Modal>
     </div>
 </template>
+<script>
+export default {
+    name:'component1',
+    data(){
+        return {
+            show:true,
+            show2:true,
+            name:'xwt'
+        }
+    }
+}
+</script>
+
