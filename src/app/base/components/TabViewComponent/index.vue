671c25f37f41ee4d665a81b3559204deafabb43d
xuweitao <2270532540@qq.com>
2018-08-07 11:13:16 +0800

    tabview 组件移动功能新增

--- src/app/base/components/TabViewComponent/index.vue
+++ src/app/base/components/TabViewComponent/index.vue
@@ -2,20 +2,23 @@
     <div class="tabview wrapper">
         <div class="tabview tab" 
           @mouseenter="showScrollBtn = true"
-          @mouseleave="showScrollBtn = false">
+          @mouseleave="showScrollBtn = false"
+          @mousewheel="mousewheel($event)">
           <i class="fa fa-chevron-left tabview-scroll scroll-left" aria-hidden="true" 
             v-if="showScrollBtn"
-            @click="scrollLeft()"></i>
+            @click="scrollLeft"></i>
           <i class="fa fa-chevron-right tabview-scroll scroll-right" aria-hidden="true" 
             v-if="showScrollBtn"
-            @click="scrollRight()"></i>
+            @click="scrollRight"></i>
           <div class="tabview-scroll-container"
             ref="scroll">
             <ul class="tabview-ul"
-              ref="tabs">
+              ref="tabs"
+              :style="scrollStyle">
               <li class="tabview-li" 
                   v-for="(componentObj,index) in tabComponents"
-                  :class="{'tabview-selected':componentObj.component == currentComponent}">
+                  :class="{'tabview-selected':componentObj.component == currentComponent}"
+                  ref="tabsList">
                   <a class="tabview-a" href="#" @click.prevent="tabClick(componentObj,index)">{{ componentObj.name }}</a>
               </li>
             </ul>
@@ -44,25 +47,51 @@
 import "./tabView.scss";
 export default {
   methods: {
-    scrollLeft(){
-      console.log(this.getTabStyle());
+    scrollLeft() {
+      let offset = this.getOffset();
+      const { scroll } = this;
+      let scrollWidth = scroll.offsetWidth;
+      let nextOffset = offset - scrollWidth;
+      this.setOffset(nextOffset > 0 ? nextOffset : 0)
     },
-    scrollRight(){
-
+    scrollRight() {
+      let offset = this.getOffset();
+      const { tabs, scroll } = this;
+      let scrollWidth = scroll.offsetWidth;
+      console.log(getComputedStyle(tabs).width);
+      let tabsWidth = tabs.offsetWidth;
+      console.log(offset,scrollWidth,tabsWidth)
+      if (scrollWidth > tabsWidth) return;
+      else {
+        //此时不需要滚动
+        if (scrollWidth + offset >= tabsWidth) return;
+        else if (tabsWidth - offset >= scrollWidth * 2) {
+          this.setOffset(scrollWidth + offset);
+        } else {
+          this.setOffset(tabsWidth - scrollWidth);
+        }
+      }
     },
-    getTabStyle(){
-      console.log(getComputedStyle(this.$refs.tabs))
+    mousewheel(event){
+      let direction = event.wheelDelta > 0 ?'left' : 'right';
+      if(direction == 'left') this.scrollLeft();
+      else this.scrollRight();
+    },
+    getTabStyle() {
+      console.log(getComputedStyle(this.$refs.tabs));
       return getComputedStyle(this.$refs.scroll);
     },
     tabClick(componentObj, index) {
       if (this.needAnimation) {
-        if(this.isAnimation || this.index == index) return;
+        if (this.isAnimation || this.index == index) return;
         this.isAnimation = true;
         this.lastIndex = this.index;
         this.index = index;
       }
       this.currentComponent = componentObj.component;
+      this.translate(index);
     },
+    translate(lis, index) {},
     beforeEnter(el) {
       if (this.needAnimation && this.lastIndex > this.index) {
         this.container.classList.add("view-animation");
@@ -71,7 +100,7 @@ export default {
       }
     },
     enter(el, done) {
-      let duration = this.getDuration();
+      let duration = this.getDuration(this.container);
       setTimeout(done, duration);
     },
     afterEnter(el) {
@@ -89,7 +118,7 @@ export default {
       }
     },
     leave(el, done) {
-      let duration = this.getDuration();
+      let duration = this.getDuration(this.container);
       setTimeout(done, duration);
     },
     afterLeave(el) {
@@ -99,12 +128,33 @@ export default {
         this.isAnimation = false;
       }
     },
-    getDuration() {
-      let computedStyle = getComputedStyle(this.container);
+    getDuration(el) {
+      let computedStyle = getComputedStyle(el);
       let transitionDelay = parseFloat(computedStyle["transitionDuration"]);
       let animationDelay = parseFloat(computedStyle["animationDuration"]);
       let duration = transitionDelay || animationDelay || 0;
       return duration * 1000;
+    },
+    setOffset(val) {
+      this.scrollStyle.transform = `translateX(${-val}px)`;
+    },
+    getOffset() {
+      let reg = /translateX\(-?(\d+(?:.\d+)?)px\)/;
+      let match = this.scrollStyle.transform.match(reg);
+      return match ? Number(match[1]) : 0;
+    },
+    activeTab(index){
+      let activeTab = this.tabsList[index];
+      let tabBound = activeTab.getBoundingClientRect();
+      let scrollBound = this.scroll.getBoundingClientRect();
+      let offset = this.getOffset();
+      if(tabBound.left < scrollBound.left){
+        let val = scrollBound.left - tabBound.left;
+        this.setOffset(offset - val);
+      }else if(tabBound.right > scrollBound.right){
+        let val = tabBound.right - scrollBound.right;
+        this.setOffset(offset + val);
+      }
     }
   },
   created() {
@@ -112,8 +162,11 @@ export default {
     currentComponentObj &&
       (this.currentComponent = currentComponentObj.component);
   },
-  mounted(){
-    this.container = this.$refs.container; 
+  mounted() {
+    this.container = this.$refs.container;
+    this.tabs = this.$refs.tabs;
+    this.tabsList = this.$refs.tabsList;
+    this.scroll = this.$refs.scroll;
   },
   props: {
     tabComponents: {
@@ -135,8 +188,15 @@ export default {
       index: 0,
       lastIndex: -1,
       isAnimation: false,
-      container:null,
-      showScrollBtn:false
+      container: null,
+      tabs: null,
+      scroll: null,
+      showScrollBtn: false,
+      offsetLeft: 0,
+      tabsList: null,
+      scrollStyle: {
+        transform: "translateX(0px)"
+      }
     };
   },
   computed: {
@@ -147,6 +207,11 @@ export default {
     },
     hasComponent() {
       return this.tabComponents.length > 0;
+    }
+  },
+  watch:{
+    index(value){
+      this.activeTab(value);
     }
   }
 };
