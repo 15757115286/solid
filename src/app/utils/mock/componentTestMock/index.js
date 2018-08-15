2cd0226ed4dc6961a04638c89ed8b90b325d0865
15757115286 <2270532540@qq.com>
2018-08-15 20:47:26 +0800

    mock使用自定义random

--- src/app/utils/mock/componentTestMock/index.js
+++ src/app/utils/mock/componentTestMock/index.js
@@ -1,10 +1,19 @@
 if (process.env.NODE_ENV == 'development') {
     let Mock = require('../config.js').default;
 
+    Mock.Random.extend({
+        getJob:function(){
+            let all =  ['coder', 'doctor', 'teacher', 'student'];
+            let random = Math.random() * all.length >> 0;
+            return all[random];
+        }
+    })
+
     Mock.mock(/\/mock\/test/, {
         'name': /[\u4e00-\u9fa5]{2,4}/,
         'sex|1': true,
-        'lang|1-4': [{'job|1':['javascript', 'java', 'c++', '.net']}],
-        'email':'@email'
+        'lang|1': ['javascript', 'java', 'c++', '.net'],
+        'email':'@email',
+        'job':'@getJob'
     })
 }
